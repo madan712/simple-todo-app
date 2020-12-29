712d05a2e4f059dc8688e0a6e1f57c672d913b81
Madan <madan712@gmail.com>
2020-12-29 18:28:56 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { ActionButton } from 'react-native-material-ui';
+import {Header, Icon } from 'react-native-elements';
+
+const todoList = [
+  {
+    id: '1',
+    title: 'First Item',
+  },
+  {
+    id: '2',
+    title: 'Second Item',
+  },
+  {
+    id: '3',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+	<View style={styles.text}>
+		<Text style={styles.title}>{title}</Text>
+	</View>
+	<View style={styles.icons}>
+		<Icon name='edit' onPress={() => console.log('edit')} />
+	</View>
+	<View style={styles.icons}>
+		<Icon name='delete' onPress={() => console.log('delete')} />
+	</View>
+  </View>
+);
+
+const renderItem = ({ item }) => (
+    <Item title={item.title} />
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header
+		  centerComponent={{ text: 'TODO', style: { color: '#fff' } }}
+		/>
+		<FlatList
+			data={todoList}
+			renderItem={renderItem}
+			keyExtractor={item => item.id}
+		  />
+		  <ActionButton onPress={() => { console.log("Add")}}/>
     </View>
   );
 }
@@ -15,7 +57,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  item: { 
+    backgroundColor: '#f9c2ff',
+    padding: 10,
+    marginVertical: 10,
+    marginHorizontal: 10,
+	flexDirection: 'row',
+	justifyContent: 'space-between',
+	borderColor: '#000',
+	borderWidth: 1,
+	borderRadius: 10
+  },
+  text: {
+	  flex: 3,
+  },
+  title: {
+	color: '#000',
+    fontSize: 20,
+  },
+  icons: {
+	flexDirection: 'row',
+	flex: 1,
+	justifyContent: 'flex-end',
+  }
 });
